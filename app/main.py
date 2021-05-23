7261c83dfd493a5326069b82ef31e5be19652bfc
Gökhan Gerdan <gokhang1327@gmail.com>
2021-05-23 21:41:22 +0300

    docker compose added for api rqworker and redis

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ from flask_rq2 import RQ
 import requests
 
 app = Flask(__name__)
-app.config['RQ_REDIS_URL'] = 'redis://127.0.0.1:6379/0'
+app.config['RQ_REDIS_URL'] = 'redis://redis:6379/0'
 rq = RQ(app)
 
 
 @rq.job
 def add():
-    res = requests.post("http://localhost:8080/test")
+    res = requests.post("http://0.0.0.0:8080/test")
     if res.status_code == 200:
         print(res.json())
 
